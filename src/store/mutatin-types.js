7134e478d7db8757b41a372c8736b21ae960e075
ThanksXly <328704719@qq.com>
2019-10-10 15:45:43 +0800

    补充注解

--- src/store/mutatin-types.js
+++ src/store/mutatin-types.js
@@ -6,15 +6,15 @@ export const RECEVIE_CATEGORYS = 'recevie_categorys';//接收食品分类列表
 export const RECEVIE_SHOPS = 'recevie_shops';//获取商铺列表
 export const RECEVIE_USER_INFO ='recevie_user_info';//获取用户信息
 export const RESET_USER_INFO = 'reset_user_info';//重置用户信息
-export const RECEVIE_INFO ='recevie_info';//
-export const RECEVIE_GOODS ='recevie_goods';//
-export const RECEVIE_RETINGS ='recevie_retings';//
+export const RECEVIE_INFO ='recevie_info';//接收用户信息
+export const RECEVIE_GOODS ='recevie_goods';//接受商品
+export const RECEVIE_RETINGS ='recevie_retings';//接受评论
 
-export const ADD_FOOD_COUNT ='add_food_count';//
-export const REDUCE_FOOD_COUNT ='reduce_food_count';
+export const ADD_FOOD_COUNT ='add_food_count';//添加食物1个
+export const REDUCE_FOOD_COUNT ='reduce_food_count';//减少食物1个
 
-export const EMPTY_CART = 'empty_cart';
+export const EMPTY_CART = 'empty_cart';//清空购物车
 
-export const RECIVIE_SEARCH_SHOPS = 'recivie_search_shops';
+export const RECIVIE_SEARCH_SHOPS = 'recivie_search_shops';//关键字搜索商品
 
 
